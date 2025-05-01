db1dbe1f2a3cc94104e546890a9611965eb32614
Ashutosh Tiwari <140781443+Ashutosh-8055-Tiwari@users.noreply.github.com>
2025-05-01 11:22:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   experimental: {
     instrumentationHook: false, 
